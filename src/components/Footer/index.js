63a2eef92dad4e48cc2abc28c02dcee49aeb973a
Swapnil Rawat <xaviergreek2000@gmail.com>
2020-12-01 14:52:33 +0530

    image fix

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import './index.scss';
 
 const Footer = () =>{
-
+  var date = new Date();
+  var year = date.getFullYear();
   return(
     <div className="footer">
+      <div className="footer-container">
+        <div className="caption">{`${year}` + ' © ' + ' Copyright'}</div>
+      </div>
     </div>
   )
 
