aa7069772b5aaea74641e24c2346ab8bfb8ea7e8
swapnilr17 <swapnilr1700@gmail.com>
2021-01-21 11:21:41 +0530

    Fixed hover

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ const Footer = () =>{
     <div className="footer">
       <div className="footer-container">
         <div className="caption">{'Swapnil Rawat'+' © '+`${year} `}</div>
-        <div className="caption-second"><a href="http://www.instagram.com/swapnilr17">Instagram</a><a href="http://www.instagram.com/swapnilr17">github</a></div>
+        <div className="caption-second"><a href="http://www.instagram.com/swapnilr17">Instagram</a><a href="http://www.github.com/swapnilr17">github</a></div>
       </div>
     </div>
   )
